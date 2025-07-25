87fa4a632024c582d41bf7a1e36e886659e6bb67
Frfjhfghv <margarettedessources42@gmail.com>
2025-07-25 00:59:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
 })
 app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
+res.sendFile(__path + '/pair.html')
 })
 app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
+res.sendFile(__path + '/pair.html')
 })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
